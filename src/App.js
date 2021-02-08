10984525243667f6ff66851cab8890aa5e25878d
Tim La <email@example.com>
2021-02-08 10:30:16 -0500

    chat engine working on application

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <ChatEngine
+      height='100vh'
+      projectID='1c5412ab-8b69-441b-b8a5-efa9de6ecde3'
+      userName='tla111'
+      userSecret='123123'
+    />
+  );
 };
 
 export default App;
